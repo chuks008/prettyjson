8cf7b8f2f63a07a3be72406573d5fc5150445a9e
chuks008 <omega.help008@gmail.com>
2018-03-28 17:36:03 +0100

    Add prompt to make sure users want to really proceed with the pretty json operation

--- prettyjson.sh
+++ prettyjson.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
+params_msg="You are about to format the following .json files in this directory:"
+all_msg="You are about to format all the .json files in this directory. Would you like to continue? Y/n"
+proceed_msg="Would you like to continue? Y/n"
+
 function beautify_json_params() {
 
+  echo $params_msg
+  for file in "$@";
+  do
+    echo "$file"
+  done
+
+  echo $proceed_msg
+  read option
+
+  while [[ $option != "Y" && $option != "y" && $option != "N" && $option != "n" ]]; do
+    echo $params_msg
+    for file in "$@";
+    do
+      echo "$file"
+    done
+
+    echo $proceed_msg
+    read option
+  done
+
+  # input requested until its between Y/y or N/n
+
+  if [[ $option == "Y" || $option == "y" ]]; then
     for file in "$@";
     do
         [ -e "$file" ] || continue
@@ -10,9 +37,23 @@ function beautify_json_params() {
         rm  copy-"$file"
         echo "$file"
     done
+  elif [[ $option == "N" || $option == "n" ]]; then
+    echo "prettyjson exited"
+    exit 1
+  fi
 }
 
 function beautify_json_all() {
+
+  echo $all_msg
+  read option
+
+  while [[ $option != "Y" && $option != "y" && $option != "N" && $option != "n" ]]; do
+    echo $all_msg
+    read option
+  done
+
+  if [[ $option == "Y" || $option == "y" ]]; then
     for file in *.json;
     do
         [ -e "$file" ] || continue
@@ -21,12 +62,14 @@ function beautify_json_all() {
         rm  copy-"$file"
         echo "$file"
     done
+  elif [[ $option == "N" || $option == "n" ]]; then
+    echo "prettyjson exited"
+    exit 1
+  fi
 }
 
 if [ -z "$1" ]; then
-    echo "No paramaters. Beautify all json files"
     beautify_json_all
 else
     beautify_json_params "$@"
 fi
-
